ed7073a9363dcf181187614658c3e3d9ccaa544d
sergiohc <sergioh.c@live.com>
2018-08-15 19:01:27 -0300

    problem vue app

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,31 +1,31 @@
 <template>
- <v-app>
-  <h4 class="white--text">Header Component</h4>
-  <v-content>
-   <h4 class="white--text">Main Component</h4>
-  </v-content>
-  <Footer/>
- </v-app>
+  <v-app>
+    <Header/>
+    <v-content>
+      <h4 class="white--text">Main Component</h4>
+    </v-content>
+    <Footer/>
+  </v-app>
 </template>
- 
+
 <script>
-  import Header from './components/shared/header.vue';
   import Footer from './components/shared/footer.vue';
- 
+  import Header from './components/shared/header.vue';
   export default {
-   data () {
-    return {
-   }
-  },
-  components: {
-   'Footer': Footer,
-   'Header': Header
+    data () {
+      return {
+      
+      }
+    },
+    components: {
+      'Footer': Footer,
+      'Header': Header
+    }
   }
- }
 </script>
- 
+
 <style>
- #app {
-  background-color: #0c0c0c !important;
- }
+  #app {
+    background-color: #0c0c0c !important;
+  }
 </style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,15 @@
-import Vue from 'vue/dist/vue.esm'
+
+import Vue from 'vue'
 import App from './app.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
+
 Vue.use(Vuetify)
- 
+
 document.addEventListener('DOMContentLoaded', () => {
- const app = new Vue({
-  el: '#app',
-  render: h => h(App)
- })
+  const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+  })
 })
