d1dee77c72445bf3999c050e3f2ef8e2fe861408
sergiohc <sergioh.c@live.com>
2018-08-15 00:52:31 -0300

    add components header and footer

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,22 +1,31 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+ <v-app>
+  <h4 class="white--text">Header Component</h4>
+  <v-content>
+   <h4 class="white--text">Main Component</h4>
+  </v-content>
+  <Footer/>
+ </v-app>
 </template>
-
+ 
 <script>
-export default {
-  data: function () {
+  import Header from './components/shared/header.vue';
+  import Footer from './components/shared/footer.vue';
+ 
+  export default {
+   data () {
     return {
-      message: "Hello Vue <3"
-    }
+   }
+  },
+  components: {
+   'Footer': Footer,
+   'Header': Header
   }
-}
+ }
 </script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+ 
+<style>
+ #app {
+  background-color: #0c0c0c !important;
+ }
 </style>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
 import App from './app.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
  
 document.addEventListener('DOMContentLoaded', () => {
  const app = new Vue({
