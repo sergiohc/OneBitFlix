57046a12424c55ba0391acbbd5d2901be219cc17
sergiohc <sergioh.c@live.com>
2018-08-19 02:07:28 -0300

    add component movie list

--- app/javascript/packs/components/home/index.vue
+++ app/javascript/packs/components/home/index.vue
@@ -1,6 +1,10 @@
-   <template>
+<template>
      <div>
         <Featured :movie="featuredMovie"/>
+        <MovieList v-for='(category, index) in categories'
+                    :key='index'
+                    :name='category.name'
+                    :movies='category.movies'/>
      </div>
    </template>
 
@@ -12,15 +16,48 @@
                             description: 'Saber como criar e consumir API’s é fundamental para qualquer programador, então nessa pequena série nós vamos ver o que é essencial para criar uma usando RoR.',
                             featured_thumbnail_url: 'https://onebitcode.com/wp-content/uploads/2018/05/bg-example.png'
                           }
+    const categoriesMock = [
+      {
+        name: "RoR", movies: [
+          {id: '1', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '2', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '3', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '4', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '5', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '6', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'}
+        ]
+      },{
+        name: "VueJS", movies: [
+          {id: '1', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '2', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '3', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '4', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '5', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '6', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'}
+        ]
+      },{
+        name: "Chatbots", movies: [
+          {id: '1', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '2', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '3', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '4', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '5', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'},
+          {id: '6', thumb_url: 'https://onebitcode.com/wp-content/uploads/2018/05/capa-v-rails-admin-1.png'}
+        ]
+      }
+    ];
     import Featured from './_featured.vue'
+    import MovieList from './_movie_list.vue'
      export default {
        data () {
          return {
             featuredMovie: featuredMovie,
+            categories: categoriesMock
          }
        },
        components: {
-         'Featured': Featured
+         'Featured': Featured,
+         'MovieList': MovieList
        }
      }
    </script>
