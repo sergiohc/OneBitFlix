48bf5ce53bbda092ead9c37600d11f7bc74cdb6a
sergiohc <sergioh.c@live.com>
2018-08-20 00:16:53 -0300

    add movie_menu_vuex

--- app/javascript/packs/components/home/_movie_list.vue
+++ app/javascript/packs/components/home/_movie_list.vue
@@ -4,91 +4,96 @@
       <v-flex xs12>
         <p class='name'>{{ name }}</p>
         <slick ref="slick" :options="slickOptions">
-          <a v-for="movie in movies"
-              :key="movie.id" 
-              href="#"
-              @click="openDetails($event, movie.id)"
-              ><img :src="movie.thumb_url">
+          <a v-for="movie in movies" :key="movie.id" href="#" @click="openDetails($event, movie.id)"><img :src="movie.thumb_url">
           </a>
         </slick>
       </v-flex>
     </v-layout>
-    <MovieMenu v-if="menuOpen" :id="movieId" :type="movieType" :closeDetails="closeDetails"/>
+    <MovieMenu v-if="menuOpen && currentMovieId == movieId" :movieId="movieId" :id="movieId" :type="movieType" :closeDetails="closeDetails" />
   </div>
 </template>
 
 <script>
-  import MovieMenu from './_movie_menu.vue';
-  import Slick from 'vue-slick';
-  export default {
-    props:  {
-              name: {
-                type: String,
-                required: true,
-              },
-              movies: {
-                type: Array,
-                required: true,
-              }
-            },
-    data () {
-      return {
-        menuOpen: false,
-        movieId: null,
-        movieType: 'serie',
-        slickOptions: {
-          slidesToShow: 4,
-          arrows: false,
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+import MovieMenu from './_movie_menu.vue';
+import Slick from 'vue-slick';
+import { mapState } from 'vuex';
+import { mapActions } from 'vuex';
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    movies: {
+      type: Array,
+      required: true,
+    }
+  },
+  data () {
+    return {
+      menuOpen: false,
+      movieId: null,
+      movieType: 'serie',
+      slickOptions: {
+        slidesToShow: 4,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
             }
-          ]
-        }
-      }
-    },
-    components: {
-      Slick,
-      MovieMenu
-    },
-    methods: {
-      openDetails(e, id) {
-        e.preventDefault();
-        if(this.menuOpen == true && this.movieId == id){
-          this.closeDetails();
-        }else {
-          this.movieId = parseInt(id);
-          this.menuOpen = true;
-        }
-      },
-      closeDetails(){
-        this.menuOpen = false;
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       }
     }
-  }
+  },
+  components: {
+    Slick,
+    MovieMenu
+  },
+  methods: {
+    openDetails (e, id) {
+      e.preventDefault();
+      if (this.menuOpen == true && this.movieId == id) {
+        this.closeDetails();
+      } else {
+        this.changeId(id);
+        this.movieId = parseInt(id);
+        this.menuOpen = true;
+      }
+    },
+    closeDetails () {
+      this.menuOpen = false;
+    },
+    ...mapActions({
+      changeId: 'MovieMenu/changeId',
+    })
+  },
+  computed: mapState({
+    currentMovieId: state => state.MovieMenu.currentMovieId,
+  })
+}
 </script>
 
 <style scoped>
-  .movie_list{
-    margin-left: 4%;
-    margin-top: 30px;
-    margin-bottom: 20px;
-  }
-  .name {
-    font-size: 24px !important;
-    font-family: 'Source Sans Pro';
-    font-weight: 600;
-    color: white;
-  }
+.movie_list {
+  margin-left: 4%;
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
+.name {
+  font-size: 24px !important;
+  font-family: "Source Sans Pro";
+  font-weight: 600;
+  color: white;
+}
 </style>
--- app/javascript/packs/store/index.js
+++ app/javascript/packs/store/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
-
+ 
+import MovieMenu from './modules/movie_menu';
+ 
 const store = new Vuex.Store({
-  modules: {}
+ modules: {
+  MovieMenu
+ }
 });
-
+ 
 export default store;
